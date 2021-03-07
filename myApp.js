de1448b094e7ee280ce075c2ecb5007933341a07
Douglas <dogge20@hotmail.com>
2021-03-07 21:30:19 +0100

    return html file

--- myApp.js
+++ myApp.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+app.get("/json", (req, res) => {
+    res.json({ "message": "Hello json" })
+})
+
 app.use("/public", express.static(__dirname + "/public"))
 
 module.exports = app;
