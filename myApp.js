2d3cb93999f5a6c16634f4b153c2f47f51624588
Douglas <dogge20@hotmail.com>
2021-03-16 12:22:35 +0100

    dotenv

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/json", (req, res) => {
-    res.json({ "message": "HEJHEJ" })
+    res.json({ "message": str.toUpperCase() })
 })
 
 app.use("/public", express.static(__dirname + "/public"))
