a935717901f46e77d9731e3ae59a34dc7a258a55
Douglas <dogge20@hotmail.com>
2021-03-16 12:20:12 +0100

    dotenv

--- myApp.js
+++ myApp.js
@@ -2,23 +2,17 @@ var express = require('express');
 var app = express();
 require('dotenv').config()
 
-
 let str = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO WORLD" : "Hello json";
 
 console.log(process.env.MESSAGE_STYLE)
 console.log(process.env.APP_ENV)
-console.log(process.env)
-
-
-console.log("Hello World")
+console.log(str)
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
 app.get("/json", (req, res) => {
-
-
     res.json({ "message": str })
 })
 
