0ee476179ba9776dd76502cc1a3b93b178450535
Douglas <dogge20@hotmail.com>
2021-03-21 18:26:39 +0100

    hej

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ console.log(process.env.MESSAGE_STYLE)
 console.log(process.env.APP_ENV)
 console.log(str)
 
+
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
@@ -16,5 +18,9 @@ app.get("/json", (req, res) => {
 })
 
 app.use("/public", express.static(__dirname + "/public"))
+
+app.use((req, res, next) => { console.log(req.method + " " + req.path + " - " + req.ip); next() })
+
+
 
 module.exports = app;
